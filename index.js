fd26185fb345bf5e56f13e5571e85003567becb3
SKrill <princedev432@gmail.com>
2021-04-26 19:43:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  
+  
+   //const userAgent = request.headers.get("User-Agent") || ""
+    // if (!userAgent.includes("fuckyouflixtvplayer")) {
+   //          let url = `https://cdn.movieforu.workers.dev/v.m3u8?file_code=fkuiyu09trij&q=n`;
+   //          return Response.redirect(url, 302);
+   //      }
+ 
+  let URLT = new URL(request.url);
+  let file_code = URLT.searchParams.get("channel")
+  let q = URLT.searchParams.get("q")
+  let url = "https://livetvkyte.herokuapp.com/"+file_code
+
+  let res = await fetch(url);
+  let obj = await res.json(); 
+
+  var finalURL = obj[q].url;
+  return Response.redirect(finalURL, 301);
+}
